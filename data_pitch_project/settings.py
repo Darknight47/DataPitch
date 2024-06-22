0f1af5cd5d1fe9109c59f4497b869da58f1cb783
Darknight47 <farhadgunners@gmail.com>
2024-06-22 12:10:44 +0200

    data_science first path

--- data_pitch_project/settings.py
+++ data_pitch_project/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My Own APPS
+    'data_science',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
