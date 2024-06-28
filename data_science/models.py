6e8a0c701809356a98cd01c925b6c84519282858
Darknight47 <farhadgunners@gmail.com>
2024-06-28 19:27:54 +0200

    new models for ds

--- data_science/models.py
+++ data_science/models.py
@@ -27,15 +27,25 @@ class DataScienceConcept(models.Model):
     def __str__(self):
         return self.ds_concept_title
 
-class DataScienceTopic(models.Model):
+class ConceptTopic(models.Model):
     concept = models.ForeignKey(DataScienceConcept, on_delete=models.CASCADE, related_name='datascience_topic')
     topic_title = models.CharField(max_length=200)
     date_added = models.DateField(auto_now_add=True)
     topic_slug = models.SlugField(blank=True)
-    
+
     class Meta:
-        verbose_name = "DataScience Topic"
-        verbose_name_plural = "DataScience Topics"
+        verbose_name = "Concept Topic"
+        verbose_name_plural = "Concept Topics"
     
     def __str__(self):
         return self.topic_title
+
+class ConceptSubtopic(models.Model):
+    topic = models.ForeignKey(ConceptTopic, on_delete=models.CASCADE, related_name='datascience_topic')
+    subtopic_title = models.CharField(max_length=200)
+    date_added = models.DateField(auto_now_add=True)
+    subtopic_slug = models.SlugField(blank=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.subtopic_title
